3e4473e8d0cdfc75e2c77080a5b51606bbefd22a
cch0618 <42212034+cch0618@users.noreply.github.com>
2019-09-12 21:47:22 +0900

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+  public String solution(String s) {
+      int length = s.length();
+      int index = length /2;
+      return (length%2==0) ? s.substring(index - 1, index + 1) : s.substring(index,index +1);
+       }
+      public static void main(String[] args)
+      {
+          Solution st = new Solution();
+         st.solution("abcde");
+          st.solution("qwer");
+     
+  }
+}
